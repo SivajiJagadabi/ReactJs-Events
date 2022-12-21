bdd32592b3ea228cb9befd5eeb7f188fd491e6c9
siva <trailnew112@gmail.com>
2022-12-21 10:58:19 +0000

    Events App

---
+++ src/components/EventItem/index.js
@@ -0,0 +1,24 @@
+// Write your code here
+import './index.css'
+
+const EventItem = props => {
+  const {eventDetails, activeEventId, isActive} = props
+  const {id, imageUrl, name, location} = eventDetails
+
+  const activeImageClassName = isActive ? `event-img active-event` : `event-img`
+
+  const setActiveId = () => {
+    activeEventId(id)
+  }
+  return (
+    <li className="event-card">
+      <button type="button" onClick={setActiveId} className="button">
+        <img src={imageUrl} alt="event" className={activeImageClassName} />
+      </button>
+      <p className="name">{name}</p>
+      <p className="location">{location}</p>
+    </li>
+  )
+}
+
+export default EventItem
